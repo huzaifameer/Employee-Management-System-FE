1d003e6decb6fe52cab8e0b22b8d9c98f6e358d9
Huzaifa Ameer <113491165+huzaifameer@users.noreply.github.com>
2024-07-01 09:01:40 +0530

    front post UI done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ManageEmpComponent } from "./page/manage-emp/manage-emp.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, ManageEmpComponent]
 })
 export class AppComponent {
   title = 'emp_system';
